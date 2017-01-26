a458c618fc4a69d1fa259565a3e01eafd1ef8fde
thuc <nguyen.van.thuc.framgia2016@gmail.com>
2017-01-26 09:44:58 +0700

    Two way binding

--- public/angular/app/app.module.ts
+++ public/angular/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { FormComponent } from './body/form/form.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, HeaderComponent, BodyComponent, FooterComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HeaderComponent, BodyComponent, FooterComponent, FormComponent ],
   bootstrap:    [ AppComponent ]
 })
 
